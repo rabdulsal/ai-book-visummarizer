07fe5c9e9c103c0ea716105b0abef8c94b2c8449
Rashad Abdul-Salaam <rabdulsalaam@gmail.com>
2025-01-14 23:34:24 -0500

    Update app.py for deployment

--- app.py
+++ app.py
@@ -300,4 +300,5 @@ def publish():
     return redirect(url_for('index', success="Video successfully published!"))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(debug=True)
